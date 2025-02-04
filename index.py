4185e698db472fd2246d724a5cf6b08f5080cb4f
Bhuvanesh-23 <bhuvaneshm.ai@gmail.com>
2025-02-04 21:06:01 +0530

    hosting

--- index.py
+++ index.py
@@ -48,4 +48,5 @@ def translate(query: str) -> str:
     return translated_text or to_Tamil(SYNTAX_ERROR)  # Change to Tamil translation
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    from waitress import serve  # type: ignore # Production WSGI server
+    serve(app, host="0.0.0.0", port=5000)
